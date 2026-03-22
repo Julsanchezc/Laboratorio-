f665a2945ca32640a0891a449cab731bbc7ff03a
Julian Santiago Sanchez Castro <julsanchezc@unal.edu.co>
2026-03-22 12:08:35 -0500

    Ampliación de pruebas de rendimiento en la clase Main
    Se mejoraron las pruebas de rendimiento de ListaSimple mediante la incorporación de nuevas operaciones y el uso de tamaños de entrada más grandes, con el fin de obtener mediciones más representativas.

--- ListaSimple/Main.java
+++ ListaSimple/Main.java
@@ -1,25 +1,123 @@
 package ListaSimple;
 
-public class Main 
-{
-    public static void main(String[] args) 
-    {
-        int[] sizes = {10,100,10000,100000};
-        for (int size: sizes)
-        {
-            ListaSimple lista = new ListaSimple();
-            long start = System.nanoTime();
-            for (int i = 0; i < size; i++) lista.pushFront(i);
-            long end = System.nanoTime();
-            System.out.println("Size " + size + " - pushFront: " + (end - start) / 1e6 + " ms");
+import java.util.Random;
 
-            ListaSimple lista2 = new ListaSimple();
-            start = System.nanoTime();
-            for (int i = 0; i < size; i++) lista2.pushBack(i);
-            end = System.nanoTime();
-            System.out.println("Size " + size + " - pushBack: " + (end - start) / 1e6 + " ms");
-            System.out.println("------------------------------------");
-        
+public class Main {
+
+    public static volatile double dummy = 0;
+
+    public static ListaSimple crearLista(int size, Random rand) {
+        ListaSimple lista = new ListaSimple();
+        for (int i = 0; i < size; i++) {
+            lista.pushBack(rand.nextDouble());
         }
+        return lista;
+    }
+
+    public static void main(String[] args) {
+
+        int[] sizes = {10, 100, 10000, 1000000, 100000000};
+        Random rand = new Random();
+        int repeticiones = 100;
+
+        for (int size : sizes) {
+
+            System.out.println("\n--- PRUEBAS PARA TAMAÑO: " + size + " ---");
+
+            ListaSimple lista;
+            double objetivo;
+
+            // -------- pushFront --------
+            lista = new ListaSimple();
+            long start = System.nanoTime();
+            for (int i = 0; i < size; i++) {
+                lista.pushFront(rand.nextDouble());
+            }
+            long end = System.nanoTime();
+            System.out.println("pushFront: " + formatTime(start, end) + " ns");
+
+            // -------- pushBack --------
+            lista = new ListaSimple();
+            start = System.nanoTime();
+            for (int i = 0; i < size; i++) {
+                lista.pushBack(rand.nextDouble());
+            }
+            end = System.nanoTime();
+            System.out.println("pushBack:  " + formatTime(start, end) + " ns");
+
+            // -------- popFront --------
+            lista = crearLista(size, rand);
+            start = System.nanoTime();
+            for (int i = 0; i < repeticiones; i++) {
+                lista.popFront();
+            }
+            end = System.nanoTime();
+            System.out.println("popFront (prom): " + (formatTime(start, end) / repeticiones) + " ns");
+
+            // -------- popBack --------
+            lista = crearLista(size, rand);
+            start = System.nanoTime();
+            for (int i = 0; i < repeticiones; i++) {
+                lista.popBack();
+            }
+            end = System.nanoTime();
+            System.out.println("popBack (prom):  " + (formatTime(start, end) / repeticiones) + " ns");
+
+            // -------- find --------
+            lista = crearLista(size, rand);
+            objetivo = rand.nextDouble();
+            lista.pushBack(objetivo);
+
+            start = System.nanoTime();
+            for (int i = 0; i < repeticiones; i++) {
+                dummy += (lista.find(objetivo) != null ? 1 : 0);
+            }
+            end = System.nanoTime();
+            System.out.println("find (prom):     " + (formatTime(start, end) / repeticiones) + " ns");
+
+            // -------- addAfter --------
+            lista = crearLista(size, rand);
+            objetivo = rand.nextDouble();
+            lista.pushBack(objetivo);
+
+            start = System.nanoTime();
+            for (int i = 0; i < repeticiones; i++) {
+                lista.addAfter(objetivo, rand.nextDouble());
+            }
+            end = System.nanoTime();
+            System.out.println("addAfter (prom): " + (formatTime(start, end) / repeticiones) + " ns");
+
+            // -------- addBefore --------
+            lista = crearLista(size, rand);
+            objetivo = rand.nextDouble();
+            lista.pushBack(objetivo);
+
+            start = System.nanoTime();
+            for (int i = 0; i < repeticiones; i++) {
+                lista.addBefore(objetivo, rand.nextDouble());
+            }
+            end = System.nanoTime();
+            System.out.println("addBefore (prom): " + (formatTime(start, end) / repeticiones) + " ns");
+
+            // -------- borrar --------
+            lista = crearLista(size, rand);
+            objetivo = rand.nextDouble();
+            lista.pushBack(objetivo);
+
+            start = System.nanoTime();
+            for (int i = 0; i < repeticiones; i++) {
+                lista.borrar(objetivo);
+            }
+            end = System.nanoTime();
+            System.out.println("borrar (prom):   " + (formatTime(start, end) / repeticiones) + " ns");
+
+            dummy = 0;
+
+            System.out.println("------------------------------------\n");
+        }
+    }
+
+    private static long formatTime(long start, long end) {
+        return (end - start); 
     }
 }
